c3f64ee047c2c6c175665b6493f82088b0c0369f
tarun2506 <tarunsharma8920@gmail.com>
2021-08-18 22:05:10 +0530

    first

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>The Way of kings</li>
+          <li>Power of your subconcious mind</li>
+          <li>The Lean Startup</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
