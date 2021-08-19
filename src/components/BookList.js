3117f07e0ba05bbe7dba10ef8a9a7f5ac9fcf315
tarun2506 <tarunsharma8920@gmail.com>
2021-08-19 21:23:03 +0530

    using functional components instead of class

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,42 +1,26 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ThemeContext } from "../context/ThemeContext";
 
-class BookList extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => {
-          return (
-            <ThemeContext.Consumer>
-              {(themeContext) => {
-                const { isAuthenticated, toggleAuth } = authContext;
-                const { isLightTheme, light, dark } = themeContext;
-                const theme = isLightTheme ? light : dark;
-                return (
-                  <div
-                    className="book-list"
-                    style={{ color: theme.syntax, background: theme.bg }}
-                  >
-                    <div onClick={toggleAuth}>
-                      {isAuthenticated ? "Logged-in" : "Logged-out"}
-                    </div>
-                    <ul>
-                      <li style={{ background: theme.ui }}>The Way of kings</li>
-                      <li style={{ background: theme.ui }}>
-                        Power of your subconcious mind
-                      </li>
-                      <li style={{ background: theme.ui }}>The Lean Startup</li>
-                    </ul>
-                  </div>
-                );
-              }}
-            </ThemeContext.Consumer>
-          );
-        }}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? "Logged-in" : "Logged-out"}
+      </div>
+      <ul>
+        <li style={{ background: theme.ui }}>The Way of kings</li>
+        <li style={{ background: theme.ui }}>Power of your subconcious mind</li>
+        <li style={{ background: theme.ui }}>The Lean Startup</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { toggleTheme } = this.context;
-    return (
-      <div>
-        <button onClick={toggleTheme}>Toggle the theme</button>
-      </div>
-    );
-  }
-}
+const ToggleTheme = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  return (
+    <div>
+      <button onClick={toggleTheme}>Toggle the theme</button>
+    </div>
+  );
+};
 
-export default ThemeToggle;
+export default ToggleTheme;
