c0e05373a9c4a3203cdd460b6c02da42be2c22ab
tarun2506 <tarunsharma8920@gmail.com>
2021-08-19 16:28:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
 import ThemeContextProvider from "./context/ThemeContext";
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,23 +2,28 @@ import React, { Component } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 class BookList extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The Way of kings</li>
-          <li style={{ background: theme.ui }}>
-            Power of your subconcious mind
-          </li>
-          <li style={{ background: theme.ui }}>The Lean Startup</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The Way of kings</li>
+                <li style={{ background: theme.ui }}>
+                  Power of your subconcious mind
+                </li>
+                <li style={{ background: theme.ui }}>The Lean Startup</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
